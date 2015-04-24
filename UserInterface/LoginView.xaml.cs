6b1ab38083592922dec0710560f98c7deb9870bf
TedMANNERs <mannersofted@gmail.com>
2015-04-24 14:16:01 +0200

    Added password hashing prototype

--- UserInterface/LoginView.xaml.cs
+++ UserInterface/LoginView.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UserInterface
 {
     /// <summary>
-    /// Interaktionslogik für LoginView.xaml
+    ///     Interaktionslogik für LoginView.xaml
     /// </summary>
     public partial class LoginView : UserControl
     {
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        private void VpnPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            LoginViewModel loginViewModel = (LoginViewModel) DataContext;
+            loginViewModel.Password = VpnPasswordBox.SecurePassword;
         }
     }
 }
--- UserInterface/LoginViewModel.cs
+++ UserInterface/LoginViewModel.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Cryptography;
 using System.Windows.Input;
 
 namespace UserInterface
@@ -11,10 +15,61 @@ namespace UserInterface
         }
 
         public ICommand LoginCommand { get; set; }
+        public SecureString Password { get; set; }
 
         private void Login()
         {
+            byte[] salt = GenerateSalt();
+            byte[] hash = Hash(salt);
             RequestSwitch(new SwitchViewEventArgs("App"));
+        }
+
+        private byte[] Hash(byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA512Cng();
+            byte[] hash = new byte[Password.Length + salt.Length];
+            byte[] passwordBytes = SecureStringToByteArray();
+
+            for (int i = 0; i < passwordBytes.Length; ++i)
+            {
+                hash[i] = passwordBytes[i];
+            }
+
+            for (int i = 0; i < salt.Length; ++i)
+            {
+                hash[passwordBytes.Length + 1] = salt[i];
+            }
+
+            return algorithm.ComputeHash(hash);
+        }
+
+        private byte[] SecureStringToByteArray()
+        {
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                byte[] result = new byte[Password.Length];
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(Password);
+                for (int i = 0; i < Password.Length; ++i)
+                {
+                    result[i] = Marshal.ReadByte(unmanagedString, i * 2);
+                    //result[i + 1] = Marshal.ReadByte(unmanagedString, i * 2 + 1);
+                }
+
+                return result;
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+
+        private byte[] GenerateSalt()
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] bytes = new byte[32];
+            rng.GetNonZeroBytes(bytes);
+            return bytes;
         }
     }
 }
